27903db0fd6a4befa837cee1a897dcf8ef3958d4
workparkso <workparkso@gmail.com>
2025-02-17 03:08:28 +0900

    update: write users functions

--- users/models.py
+++ users/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=30, unique=True) # 고유성 추가가
+    nickname = models.CharField(max_length=30, unique=True)
 
     class Meta:
-        unique_together = ('username', 'nickname')
+        constraints = [
+            models.UniqueConstraint(fields=['username', 'nickname'], name='unique_username_nickname')
+        ]
